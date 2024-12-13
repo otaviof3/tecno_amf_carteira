bbacb5439848b00723a02d3a9ccca9851d3742b0
otaviof3 <otaviofd03@gmail.com>
2024-12-13 12:32:11 -0300

    Lógica de Login

--- projeto_cart_estudante/app_cart_estudante/views.py
+++ projeto_cart_estudante/app_cart_estudante/views.py
@@ -1,25 +1,22 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from .models import Estudante
 
 def home(request):
-    return render(request, "estudantes/home.html")
-
-def perfil_estudante(request):
-    matricula = request.session.get('matricula')
-    if not matricula:
-        return redirect('login')  
-    estudante = get_object_or_404(Estudante, matricula=matricula)
-    return render(request, 'perfil_estudante.html', {'estudante': estudante})
-
-def login_view(request):
-    if request.method == "POST":
-        username = request.POST['matricula']
+    if request.method == "POST":  
+        username = request.POST['ra'] 
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('perfil_estudante') 
+            return redirect('perfil_estudante')  
         else:
-            return render(request, 'login.html', {'error': 'Credenciais inválidas.'})
-    return render(request, 'login.html')
+            return render(request, 'estudantes/home.html', {'error': 'Credenciais inválidas.'})
+    
+    return render(request, 'estudantes/home.html')  
+
+@login_required
+def perfil_estudante(request):
+    estudante = get_object_or_404(Estudante, matricula=request.user.username)
+    return render(request, 'perfil_estudante.html', {'estudante': estudante})
--- projeto_cart_estudante/projeto_cart_estudante/urls.py
+++ projeto_cart_estudante/projeto_cart_estudante/urls.py
@@ -3,6 +3,5 @@ from app_cart_estudante import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('perfil/', views.perfil_estudante, name='perfil_estudante'),
-    path('login/', views.login_view, name='login'),  
+    path('perfil/', views.perfil_estudante, name='perfil_estudante'),  
 ]
